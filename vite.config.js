d394fc1f1b434e7d5dcdea1e8dcba90b229e73c0
hoafn0730 <hoantran0730@gmail.com>
2025-06-01 12:13:29 +0700

    fix: correct path resolution in jsconfig and clean up vite config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import eslint from 'vite-plugin-eslint';
 import path from 'path';
-import { fileURLToPath } from 'url';
 
 const PORT = 3030;
 // https://vite.dev/config/
